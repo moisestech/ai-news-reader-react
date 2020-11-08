e8ef318d52058176bd98effc0d8747f266f394a9
Moises Sanabria <m@moises.tech>
2020-11-08 18:18:07 -0500

    init func comp Nav for test

---
+++ src/components/nav/index.js
@@ -0,0 +1,15 @@
+import "./nav.css";
+import { useState, useEffect } from "react";
+
+export default function Nav() {
+
+  // load environment connection with Alan AI
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div className="nav">
+      <h1>Nav</h1>
+    </div>
+  );
+}
