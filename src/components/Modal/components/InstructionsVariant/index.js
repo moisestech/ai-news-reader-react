5cedd46f444618ee6b61cc08a23c212276a94542
Moises Sanabria <m@moises.tech>
2020-11-17 07:32:27 -0500

    refactored Instructions into NewsBy Term Category Source

---
+++ src/components/Modal/components/InstructionsVariant/index.js
@@ -0,0 +1,26 @@
+/* eslint-disable react/react-in-jsx-scope */
+import useStyles from '../../styles';
+
+// COMPONENTS
+import NewsByCategory from './components/NewsByCategory';
+import NewsByTerm from './components/NewsByTerm';
+import NewsBySource from './components/NewsBySource';
+
+// MATERIAL-UI
+import { Typography, Divider } from '@material-ui/core';
+
+export default function InstructionsVariant() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.paper}>
+      <Typography variant="h3">Instructions</Typography>
+      <Divider />
+      <NewsByTerm />
+      <Divider />
+      <NewsByCategory />
+      <Divider />
+      <NewsBySource />
+    </div>
+  );
+}
