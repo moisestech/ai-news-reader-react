ca0c0db0561a35e07095aa8a17634205e9f81ee5
Moises Sanabria <m@moises.tech>
2020-11-17 07:35:13 -0500

    imp useStyles map through Term w/ Chip material-ui comp

---
+++ src/components/Modal/components/InstructionsVariant/components/NewsByTerm/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable react/react-in-jsx-scope */
+import useStyles from '../../../../styles';
+
+// MATERIAL-UI
+import { Typography, Chip } from '@material-ui/core';
+
+export default function NewsByTerm() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.infoContainer}>
+        <Typography variant="h5">News by Terms</Typography>
+        <div className={classes.chipContainer}>
+          {[
+            'Donald Trump',
+            'Bitcoin',
+            'PlayStation 5',
+            'Smartphones',
+          ].map((term, i) => (
+            <Chip
+              label={term}
+              color="primary"
+              className={classes.chip}
+              key={i}
+            />
+          ))}
+          <Chip label="...and more" className={classes.chip} />
+        </div>
+      </div>
+      <Typography variant="body1" className={classes.trySaying}>
+        Try saying: &quot;What&apos;s up with{' '}
+        <strong>
+          <em>PlayStation 5</em>
+        </strong>
+        &quot;
+      </Typography>
+    </>
+  );
+}
