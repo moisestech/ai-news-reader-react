46ef09b3652e6037a7caf045571585a0abe7f3ab
Moises Sanabria <m@moises.tech>
2020-11-17 21:04:37 -0500

    refactored array into const outisde of func comp for legibility

--- src/components/Modal/components/InstructionsVariant/components/NewsBySource/index.js
+++ src/components/Modal/components/InstructionsVariant/components/NewsBySource/index.js
@@ -4,6 +4,16 @@ import useStyles from '../../../../styles';
 // MATERIAL-UI
 import { Typography, Chip } from '@material-ui/core';
 
+const sources = [
+  'CNN',
+  'Wired',
+  'BBC News',
+  'Time',
+  'IGN',
+  'Buzzfeed',
+  'ABC News',
+];
+
 export default function NewsBySource() {
   const classes = useStyles();
 
@@ -12,15 +22,7 @@ export default function NewsBySource() {
       <div className={classes.infoContainer}>
         <Typography variant="h5">News by Sources</Typography>
         <div className={classes.chipContainer}>
-          {[
-            'CNN',
-            'Wired',
-            'BBC News',
-            'Time',
-            'IGN',
-            'Buzzfeed',
-            'ABC News',
-          ].map((source, i) => (
+          {sources.map((source, i) => (
             <Chip
               label={source}
               color="primary"
