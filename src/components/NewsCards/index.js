2ba570e855ef72bb1b9fc661611a4c1a23e4a849
Moises Sanabria <m@moises.tech>
2021-01-02 16:50:21 -0500

    debug func comp getting data from NewsAPI

--- src/components/NewsCards/index.js
+++ src/components/NewsCards/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-key */
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
 import PropTypes from 'prop-types';
 
 // COMPONENTS
@@ -7,46 +7,50 @@ import NewsCard from '../NewsCard';
 import InfoCards from '../InfoCards';
 
 // MATERIAL UI
-import { Grid, Grow, Typography } from '@material-ui/core';
+import { Grid, Grow } from '@material-ui/core';
 import useStyles from './styles.js';
 
-export default function NewsCards({ articles, activeArticle }) {
-  const classes = useStyles();
+const NewsCards = React.forwardRef(
+  ({ articles, activeArticle }, ref) => {
+    const classes = useStyles();
 
-  if (!articles.length) {
-    <InfoCards />;
-  }
+    !articles.length ? <InfoCards /> : null;
 
-  return (
-    <Grow in>
-      <Grid
-        className={classes.container}
-        container
-        alignItems="stretch"
-        spacing={3}
-      >
-        {articles.map((article, index) => (
-          <Grid
-            item
-            xs={12}
-            sm={16}
-            md={4}
-            lg={3}
-            style={{ display: 'flex' }}
-          >
-            <NewsCard
-              activeArticle={activeArticle}
-              article={article}
-              index={index}
-            />
-          </Grid>
-        ))}
-      </Grid>
-    </Grow>
-  );
-}
+    return (
+      <Grow in ref={ref}>
+        <Grid
+          className={classes.container}
+          container
+          alignItems="stretch"
+          spacing={3}
+        >
+          {articles.map((article, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={16}
+              md={4}
+              lg={3}
+              style={{ display: 'flex' }}
+            >
+              <NewsCard
+                activeArticle={activeArticle}
+                article={article}
+                index={index}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Grow>
+    );
+  },
+);
+
+NewsCards.displayName = 'NewsCards';
 
 NewsCards.propTypes = {
   articles: PropTypes.array.isRequired,
-  activeArticle: PropTypes.object.isRequired,
+  activeArticle: PropTypes.number.isRequired,
 };
+
+export default NewsCards;
